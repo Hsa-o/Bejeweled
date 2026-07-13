48ab7702462f7ffab84b7b1874dde19b3ada4a11
Hsa-o <hsijiahao7980@gmail.com>
2026-07-13 20:05:24 -0300

    Revert "Cria e Mostra as Matrizes"
    This reverts commit e1d478104a7a48073d359859db054a629a48ccf6.

--- main.c
+++ main.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "funcoes.h"
 
-void criar_matriz(int matriz[8][8]){
-    srand(time(NULL));
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            matriz[i][j] = (rand() % 5)+1;
-        }
-    }
-}
-void mostrar_matriz(int matriz [8][8]){
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            printf("%d\t", matriz[i][j]);
-        }
-        printf("\n");
-    }
-}
+int main(){
 
-int main(int argc, char const *argv[])
-{
-    int matriz [8][8];
-    criar_matriz(matriz);
-    mostrar_matriz(matriz);
+    int x, y;
+
+    printf("Digite dois numeros: ");
+    scanf("%d%d",&x,&y);
+
+    printf("Soma = %d\n", soma(x,y));
+    printf("Subtracao = %d\n", subtracao(x,y));
+    printf("Multiplicacao = %d\n", multiplicacao(x,y));
+    printf("Divisao = %.2f\n", divisao(x,y));
+
     return 0;
-}   
+}
