279248890ae9e43b777f7c52de5e4fbc8129e126
Hsa-o <hsijiahao7980@gmail.com>
2026-07-14 18:57:59 -0300

    Fazer com teste simples

--- main.c
+++ main.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-void criar_matriz(int matriz[8][8]){
-    srand(time(NULL));
-    for (int i = 0; i < 8; i++)
+void posicao_troca(int matriz[3][3]){
+    int linha, linha1, coluna, coluna1;
+    printf("Linha, coluna: ");
+    scanf("%d%d", &linha, &coluna);
+    
+    printf("Linha, coluna: ");
+    scanf("%d%d", &linha1, &coluna1);
+    printf("===================\n");
+    
+    printf("%d", matriz[linha-1][coluna-1]);
+    printf("\n");
+    printf("%d\n=================\n", matriz[linha1-1][coluna1-1]);
+    //se linha e coluna diferente de 1-4, error!
+    int copia_pos1 = matriz[linha-1][coluna-1];
+    matriz[linha-1][coluna-1] = matriz[linha1-1][coluna1-1];
+    matriz[linha1-1][coluna1-1] = copia_pos1;
+    
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < 3; j++)
         {
-            matriz[i][j] = (rand() % 5)+1;
-        }
-    }
-}
-void mostrar_matriz(int matriz [8][8]){
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            printf("%d\t", matriz[i][j]);
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
 }
+void troca(int matriz[3][3]){
+    
+    
+}
+int main (){
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 3},
+        {7, 3, 9}
+    };
 
-int main(int argc, char const *argv[])
-{
-    int matriz [8][8];
-    criar_matriz(matriz);
-    mostrar_matriz(matriz);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
+    posicao_troca(matriz);
+    
     return 0;
 }
---
+++ teste.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void criar_matriz(int matriz[8][8]){
+    srand(time(NULL));
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            matriz[i][j] = (rand() % 5)+1;
+        }
+    }
+}
+void mostrar_matriz(int matriz [8][8]){
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int matriz [8][8];
+    criar_matriz(matriz);
+    mostrar_matriz(matriz);
+    return 0;
+}
