dc20410fdfc72770564117a3ac42737ffa512785
Hsa-o <hsijiahao7980@gmail.com>
2026-07-27 23:17:57 -0300

    .

--- main.c
+++ main.c
@@ -55,35 +55,7 @@ int main(void)
         if (tela == 0)
         {
             
-                Rectangle botaoJogar = {460, 405, 334, 80};
-     DrawTexturePro
-            (
-                *menu,
-    
-                //"Qual pedaço dessa imagem eu quero usar?"
-                //0,0 que é o canto superior esquerdo ate o widht e o height da imagem(1920, 1080)
-                (Rectangle){0, 0, menu->width, menu->height},
-    
-                //"Depois de pegar esse pedaço, onde ele será desenhado e com qual tamanho?"
-                //0,0 ate o tamanho do initwindows, que é o tamanho da janela criada.
-                (Rectangle){0, 0, 1255, 723},
-                (Vector2){0, 0},
-                0,
-                WHITE
-            );
-               if (CheckCollisionPointRec(mouse, botaoJogar))
-               {
-                   DrawRectangleRec(botaoJogar, Fade(GREEN, 0.3f));
-    
-                   if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-                   {
-                     *tela = 1;
-                     criar_matriz(matriz);
-                     *jogo_iniciado = 1;
-                   }
-               }
-
-}
+                desenharMenu(&menu, mouse, &jogo_iniciado, &tela, matriz);
         }
 
         else if(tela == 1)
